8856cf2a8dff5e8649bde9c93b06c1bb753b059c
Benjamin Marquis <bmarquiscom@gmail.com>
2019-11-28 10:04:32 -0500

    add og meta tags

---
+++ next-seo.config.js
@@ -0,0 +1,24 @@
+export default {
+  title: 'Benjamin Marquis',
+  description: 'Discover some of my projects!',
+  keywords: "full-stack developer, machine learning, programming, developer, react, js, node, docker",
+  robot: 'index, follow',
+  viewport: 'width=device-width, initial-scale=1',
+  openGraph: {
+    type: 'profile',
+    locale: 'en_IE',
+    url: 'https://www.benjaminmarquis.com/',
+    title: 'Benjamin Marquis',
+    description: 'Discover some of my projects!',
+    site_name: 'Benjamin Marquis - Find some of my projects!',
+    images: [
+      {
+        url: "/static/images/ogimage.png",
+        width: "1200",
+        height: "630",
+        type: "image/png",
+        alt: "A banner open graph image with a meeting image an my avatar"
+      }
+    ]
+  }
+};
--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,8 +1,11 @@
 import React from "react";
 import App from "next/app";
+import { DefaultSeo } from 'next-seo';
 import Head from 'next/head';
 
 import WithGtmScript from '../src/components/hoc/WithGtmScript';
+
+import SEO from '../next-seo.config';
 
 import '../public/static/css/application.scss';
 
@@ -17,24 +20,9 @@ class MyApp extends App {
     const { Component, pageProps } = this.props;
     return (
       <WithGtmScript>
+        <DefaultSeo {...SEO} />
         <Head>
-          // <meta name="title" content="Benjamin Marquis" />
-          <meta name="description" content="Find some of my last projects!" />
-          <meta name="keywords" content="full-stack developer, machine learning, programming, developer, react, js, node, docker" />
-          <meta name="robots" content="index, follow" />
           <meta httpEquiv="Content-Type" content="text/html; charset=utf-8" />
-          <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
-          <meta name="language" content="English" />
-          <meta property="og:title" content="Benjamin Marquis" />
-          <meta property="og:site_name" content="benjaminmarquis.com - Find some of my projects!" />
-          <meta property="og:url" content="https://benjaminmarquis.com/" />
-          <meta property="og:description" content="Find some of my last Project!" />
-          <meta property="og:type" content="profile" />
-          <meta property="og:image" src="/static/images/ogimage.png" />
-          <meta property="og:image:type" content="image/png" />
-          <meta property="og:image:width" content="1200" />
-          <meta property="og:image:height" content="630" />
-          <meta property="og:image:alt" content="A banner open graph image with a meeting image an my avatar" />
         </Head>
         <Component {...pageProps} />
       </WithGtmScript>
