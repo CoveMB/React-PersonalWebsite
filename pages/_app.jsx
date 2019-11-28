18dabf04abae0d514ea966eed4a1965af07c3665
Benjamin Marquis <bmarquiscom@gmail.com>
2019-11-28 09:40:24 -0500

    add og meta tags

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -18,7 +18,23 @@ class MyApp extends App {
     return (
       <WithGtmScript>
         <Head>
-          <title>Benjamin Marquis</title>
+          <meta name="title" content="Benjamin Marquis" />
+          <meta name="description" content="Find some of my last projects!" />
+          <meta name="keywords" content="full-stack developer, machine learning, programming, developer, react, js, node, docker" />
+          <meta name="robots" content="index, follow" />
+          <meta httpEquiv="Content-Type" content="text/html; charset=utf-8" />
+          <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+          <meta name="language" content="English" />
+          <meta property="og:title" content="Benjamin Marquis" />
+          <meta property="og:site_name" content="benjaminmarquis.com - Find some of my projects!" />
+          <meta property="og:url" content="https://benjaminmarquis.com/" />
+          <meta property="og:description" content="Find some of my last Project!" />
+          <meta property="og:type" content="profile" />
+          <meta property="og:image" src="/static/images/ogimage.png" />
+          <meta property="og:image:type" content="image/png" />
+          <meta property="og:image:width" content="400" />
+          <meta property="og:image:height" content="300" />
+          <meta property="og:image:alt" content="A banner open graph image with a meeting image an my avatar" />
         </Head>
         <Component {...pageProps} />
       </WithGtmScript>
--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -5,8 +5,6 @@ const footer = () => {
     <div className="footer">
       <h2><a className="footerLink" href="mailto:bmarquiscom@gmail.com">bmarquiscom@gmail.com</a></h2>
       <a className="footerLink" href="https://www.linkedin.com/in/benjamin-marquis-6956b367/?locale=en_US" target="_blank"><img className="svgFooter" src="/static/images/linkedinSvg.svg" alt="LinkedIn Icon" /></a>
-      <a className="footerLink" href="https://github.com/BjMrq" target="_blank"><img className="svgFooter" src="/static/images/github.svg" alt="GitHub Icon" /></a>
-      <a className="footerLink" href="https://www.instagram.com/bjmrq/?hl=en" target="_blank"><img className="svgFooter" src="/static/images/instagram.svg" alt="Instagram Icon" /></a>
     </div>
   );
 };
