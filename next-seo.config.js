05c2a7512f3df8e4d213e547f43835dc3b016c2e
Benjamin Marquis <bmarquiscom@gmail.com>
2019-11-29 08:54:49 -0500

    added square open graph

--- next-seo.config.js
+++ next-seo.config.js
@@ -14,11 +14,18 @@ export default {
     site_name: 'Benjamin Marquis - Find some of my projects!',
     images: [
       {
-        url: "https://ruby-portfolio-bucket.s3.ca-central-1.amazonaws.com/ogimage.png",
+        url: "https://ruby-portfolio-bucket.s3.ca-central-1.amazonaws.com/ogimage2.png",
         width: "1200",
         height: "630",
         type: "image/png",
-        alt: "A banner open graph image with a meeting image an my avatar"
+        alt: "A banner open graph image with a universe picture and my avatar"
+      },
+      {
+        url: "https://ruby-portfolio-bucket.s3.ca-central-1.amazonaws.com/squareogimage.jpg",
+        width: "1200",
+        height: "1200",
+        type: "image/png",
+        alt: "A square open graph image with a universe picture and my avatar"
       }
     ]
   }
