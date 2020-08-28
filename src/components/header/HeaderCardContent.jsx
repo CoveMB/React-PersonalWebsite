adfac89f45804e4adfcce9dd6c345f40f6fee9dd
Benjamin <bmarquiscom@gmail.com>
2020-08-28 16:27:12 -0400

    Make it pwa

--- src/components/header/HeaderCardContent.jsx
+++ src/components/header/HeaderCardContent.jsx
@@ -26,9 +26,9 @@ const headerCardContent = () => {
         </p>
         <div className="social-icons-header">
           <a className="footerLink" href="mailto:bmarquiscom@gmail.com"><img className="svgSocialHeader" src="/static/images/email.svg" alt="email icon" /></a>
-          <a className="footerLink" href="https://www.linkedin.com/in/benjamin-marquis-6956b367/?locale=en_US" target="_blank"><img className="svgSocialHeader" src="/static/images/linkedinSvg.svg" alt="LinkedIn Icon" /></a>
-          <a className="footerLink" href="https://github.com/BjMrq" target="_blank"><img className="svgSocialHeader" src="/static/images/github.svg" alt="GitHub Icon" /></a>
-          <a className="footerLink" href="https://www.instagram.com/bjmrq/?hl=en" target="_blank"><img className="svgSocialHeader" src="/static/images/instagram.svg" alt="Instagram Icon" /></a>
+          <a className="footerLink" href="https://www.linkedin.com/in/benjamin-marquis-6956b367/?locale=en_US" rel="noopener noreferrer" target="_blank"><img className="svgSocialHeader" src="/static/images/linkedinSvg.svg" alt="LinkedIn Icon" /></a>
+          <a className="footerLink" href="https://github.com/BjMrq" target="_blank" rel="noopener noreferrer"><img className="svgSocialHeader" src="/static/images/github.svg" alt="GitHub Icon" /></a>
+          <a className="footerLink" href="https://www.instagram.com/bjmrq/?hl=en" target="_blank" rel="noopener noreferrer"><img className="svgSocialHeader" src="/static/images/instagram.svg" alt="Instagram Icon" /></a>
         </div>
       </div>
     </Fragment>
