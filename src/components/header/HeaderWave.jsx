998e72f546318e39a037e51592e2386a17d949cb
Benjamin <bmarquiscom@gmail.com>
2020-08-31 20:45:08 -0400

    Fix off layout

--- src/components/header/HeaderWave.jsx
+++ src/components/header/HeaderWave.jsx
@@ -1,9 +1,7 @@
 import React from 'react';
 
-const headerWave = () => {
-  return (
-    <img className="header-wave" src="/static/images/vectorheader.svg" alt="svg wave" />
-  );
-};
+const headerWave = () => (
+  <img className="header-wave" src="/static/images/vectorheader.svg" alt="svg wave" />
+);
 
 export default headerWave;
