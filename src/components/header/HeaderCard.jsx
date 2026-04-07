5c25b66853eedf2eaf76f0b1a4775967149c887a
CoveMB <bmarquiscom@gmail.com>
2026-04-07 16:28:39 -0400

    reduce motion

--- src/components/header/HeaderCard.jsx
+++ src/components/header/HeaderCard.jsx
@@ -6,7 +6,7 @@ import HeaderCardContent from "./HeaderCardContent";
 export default function HeaderCard() {
   return (
     <ScrollParallax className="parallaxTitle" parallaxData={parallaxDataHeader}>
-      <div className="header-card scrollbarxcustom animated fadeIn">
+      <div className="header-card scrollbarxcustom animated">
         <div className="gradient-square-avatar">
           <Avatar />
         </div>
